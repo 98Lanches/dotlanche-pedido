c051ff76f74cb4954df53b30c586a567bc6b04d0
Angelo Costa Neto <ang.costa.neto@gmail.com>
2024-11-09 21:54:02 -0300

    increase coverage

--- test/UnitTests/DotLanches.Pedidos.UnitTests/Core/Domain/ValueObjects/ComboTest.cs
+++ test/UnitTests/DotLanches.Pedidos.UnitTests/Core/Domain/ValueObjects/ComboTest.cs
@@ -15,5 +15,15 @@ namespace DotLanches.Pedidos.Tests.Domain.ValeObjects
             var ex = Assert.Throws<DomainValidationException>(() => new Combo(idProduto, preco));
             Assert.That(ex.Message, Is.EqualTo("invalid value for idProduto!"));
         }
+
+        [Test]
+        public void Construtor_DeveLancarExcecao_QuandoPrecoFor0()
+        {
+            var idProduto = Guid.Parse("01234567-89ab-cdef-0123-456789abcdef");
+            var preco = 0.00m;
+
+            var ex = Assert.Throws<DomainValidationException>(() => new Combo(idProduto, preco));
+            Assert.That(ex.Message, Is.EqualTo("invalid value for preco!"));
+        }
     }
 }
